38d22fb6b22403ee3b022451c785d31b5512b04e
nie xin <niexin.study@gmail.com>
2015-11-26 22:43:43 +0100

    Add rating

--- image-share/image-share.js
+++ image-share/image-share.js
@@ -1,27 +1,35 @@
 Images = new Mongo.Collection('images');
-console.log(Images.find().count());
+console.log('image-share.js', Images.find().count());
 
 if (Meteor.isClient) {
-  var img_data = [{
-    img_src: 'laptops.jpg',
-    img_alt: 'some laptops',
-  },
-  {
-    img_src: 'bass.jpg',
-    img_alt: 'a bass player',
-  },
-  {
-    img_src: 'beard.jpg',
-    img_alt: 'some musicians with beards',
-  },
-  ];
-
-  Template.images.helpers({imgs: img_data});
+  // var img_data = [{
+  //   img_src: 'laptops.jpg',
+  //   img_alt: 'some laptops',
+  // },
+  // {
+  //   img_src: 'bass.jpg',
+  //   img_alt: 'a bass player',
+  // },
+  // {
+  //   img_src: 'beard.jpg',
+  //   img_alt: 'some musicians with beards',
+  // },
+  // ];
+  //
+  Template.images.helpers({imgs: Images.find({}, {sort: {rating: -1}})});
 
   Template.images.events({
     'click .js-image': function(event) {
       $(event.target).css("width", "50px");
     },
+
+    'click .js-rate-image': function(event) {
+      var rating = $(event.currentTarget).data('userrating');
+      var img_id = this.id;
+      Images.update({_id: img_id},
+                    {$set: {rating: rating}});
+    },
+
   });
 }
 
--- my_first_app/my_first_app.js
+++ my_first_app/my_first_app.js
@@ -5,7 +5,7 @@ if (Meteor.isClient) {
   });
 
   Template.hello.events({
-    'click button': function () {
+    'click button': function() {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
     }
@@ -13,7 +13,7 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
+  Meteor.startup(function() {
     // code to run on server at startup
   });
 }
