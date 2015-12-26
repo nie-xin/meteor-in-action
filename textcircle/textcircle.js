c6447347c780f04a1b1b7be08a768777af80e92b
nie xin <niexin.study@gmail.com>
2015-12-26 12:14:55 +0100

    add editing user log to db

--- textcircle/textcircle.js
+++ textcircle/textcircle.js
@@ -1,4 +1,5 @@
 this.Documents = new Mongo.Collection('documents');
+EditingUsers = new Mongo.Collection('editingUsers');
 
 if (Meteor.isClient) {
 
@@ -21,17 +22,49 @@ if (Meteor.isClient) {
       return function(editor) {
         editor.on('change', function(cm_editor, info) {
           $('#viewer-iframe').contents().find('html').html(cm_editor.getValue());
+          Meteor.call('addEditingUser');
         });
-      }
+      };
     },
   });
 
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
+  Meteor.startup(function() {
     if (!Documents.findOne()) {
       Documents.insert({title: 'my new doc'});
     }
   });
 }
+
+Meteor.methods({
+  addEditingUser: function() {
+    var doc, user, eusers;
+    doc = Documents.findOne();
+
+    if (!doc) {
+      return;
+    };
+
+    if (!this.userId) {
+      return;
+    };
+
+    user = Meteor.user().profile;
+
+    eusers = EditingUsers.findOne({docid: doc._id});
+
+    if (!eusers) {
+      eusers = {
+        docid: doc._id,
+        users: {},
+      };
+    }
+
+    user.lastEdit = new Date();
+    eusers.users[this.userId] = user;
+
+    EditingUsers.upsert({_id: eusers._id}, eusers);
+  }
+});
